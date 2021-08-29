d6bbd3bf785ff1425b44ca3cc86316cc2ef9a55e
Felipe <felipe@superclient.com.br>
2021-08-29 00:01:19 -0300

    jogo funcional

--- liga4.py
+++ liga4.py
@@ -51,22 +51,35 @@ class Liga4:
         self.clock = pygame.time.Clock()
         
         self.jogador_atual = Jogadores.JOGADOR1
-    
-    def play_step(self):
-        return '',''
-    
+        self.rodada = 1 
+        self.vencedor = ''
+        
     def jogada(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
                 
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                colocada = self.colocar_peca(pos)
+                if colocada: self.trocar_jogador()
         
+        acabou, vencedor = self.verificarVitoria()
         self.mostrarTablueiro()
         pygame.display.update()
         self.clock.tick(SPEED)
 
-        acabou, vencedor = self.verificarVitoria()
+            
+        if self.rodada == 49:
+            text = font.render(f"Empate", True, BRANCO)
+            self.display.blit(text, [TAMANHO_TABULEIRO//2, TAMANHO_TABULEIRO//2])
+            print(f"Empate")
+
+        if acabou:
+            self.vencedor = vencedor
+            
+            
         return acabou, vencedor
 
     def mostrarTablueiro(self):
@@ -98,31 +111,62 @@ class Liga4:
                 y_peca += TAMANHO_PECA + 5
             x_peca += TAMANHO_PECA + 5
             
+        text = font.render(f"{self.jogador_atual.name}", True, BRANCO)
+        self.display.blit(text, [0, 0])
+        
+        if self.vencedor:
+            text = font.render(f"O {self.vencedor} Ganhou!", True, BRANCO)
+            self.display.blit(text, [TAMANHO_TABULEIRO//2, TAMANHO_TABULEIRO//2])
+            
     def verificarVitoria(self):
         """ Verifica se alguem ganhou """
-        global CONTADOR
         for i in range(len(TABULEIRO)):
             for j in range(i):
                 casa = TABULEIRO[i][j]
                 if casa == 0:
-                    CONTADOR = CONTADOR + 1
                     continue
                 if j <= 3 and casa == TABULEIRO[i][j+1] == TABULEIRO[i][j+2] == TABULEIRO[i][j+3]:
-                    CONTADOR = CONTADOR + 1
                     return True, casa
                 if i <= 3 and casa == TABULEIRO[i+1][j] == TABULEIRO[i+2][j] == TABULEIRO[i+3][j]:
-                    CONTADOR = CONTADOR + 1
                     return True, casa
                 if i <= 3 and j <= 3 and casa == TABULEIRO[i+1][j+1] == TABULEIRO[i+2][j+2] == TABULEIRO[i+3][j+3]:
-                    CONTADOR = CONTADOR + 1
                     return True, casa
                 if i >= 3 and j < 7 and j <= 3 and casa == TABULEIRO[i-1][j+1] == TABULEIRO[i-2][j+2] == TABULEIRO[i-3][j+3]:
-                    CONTADOR = CONTADOR + 1
                     return True, casa
         return False, ''
         
+    def colocar_peca(self, pos):
+        x, y = pos
+        print(pos)
+        
+        casa = 0
+        for i in range(len(TABULEIRO)):
+            print(i, (TAMANHO_PECA + 5)*i)
+            if (TAMANHO_PECA + 5) * (i + 1) >= x:
+                casa = i
+                break
+        
+        tamanho = len(TABULEIRO) - 1
+        
+        for i in range(len(TABULEIRO)):
+            linha = tamanho-i
+            if TABULEIRO[tamanho-i][casa] == 0:
+                TABULEIRO[tamanho-i][casa] = self.jogador_atual.value
+                return True
+            
+        print('Esta coluna está cheia!')
+        return False
+    
+    def trocar_jogador(self):
+        self.jogador_atual = JOGADORES[self.rodada % 2]
+        self.rodada += 1
+    
+    
 if __name__ == '__main__':
     game = Liga4()
 
     while True:
-        game.jogada()
+        acabou, vencedor = game.jogada()
+        if acabou: 
+            time.sleep(10)
+            break
